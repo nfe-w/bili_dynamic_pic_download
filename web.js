a68a4fe0f24bfdc694e57c2d90b22ec78fbb6e6d
nfe-w <nfe-w@outlook.com>
2023-09-02 17:52:52 +0800

    fix

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 const doGetUrl = require("./getBiliDynamicPicUrl");
 
 var express = require("express");
+const bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json());
 
 app.all("/getBiliDynamicPicUrl", async function (req, res) {
   res.set("Content-Type", "application/json");
-  const shareUrl = req.query.shareUrl;
+  const shareUrl = req.query.shareUrl || req.body.shareUrl;
   if (!shareUrl) {
     res.send(JSON.stringify({
       error: "缺少shareUrl参数",
